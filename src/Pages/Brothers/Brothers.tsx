49cc0b05c58481e7ddd0abae9d1924ec7e256a7b
Aaron_Liang <aaronhliang@gmail.com>
2020-11-20 09:52:37 -0800

    added dropdown button

--- src/Pages/Brothers/Brothers.tsx
+++ src/Pages/Brothers/Brothers.tsx
@@ -1,19 +1,32 @@
-// import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, Dropdown, DropdownButton } from "react-bootstrap";
 import React, { Fragment } from "react";
 import "./Brothers.css"
 
-export default class Brothers extends React.Component<{}, { selected: string }> {
+export default class Brothers extends React.Component {
     constructor(props: {}) {
         super(props);
     }
 
-
     render() {
         return (
             <Fragment>
-               <section>  Brothers Page</section>
+               <div><SelectPage/> </div>
             </Fragment>
         );
     }
 }
+
+class SelectPage extends React.Component {
+    classes = ["Founding", "Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota", "Kappa", "Lambda",
+                "Mu", "Nu", "Xi", "Omicron", "Pi", "Rho", "Tau"]
+
+    render() {
+        return (
+            <DropdownButton id="button" title="Class">
+                {this.classes.reverse().map((class_name) =>  <Dropdown.Item href="#/action-1">{class_name}</Dropdown.Item>)}
+            </DropdownButton>
+        );
+    }
+}
+
 
