315dc9925071bc39b1c16808b84d55e3e52eb103
Radicitus <novivolatus@gmail.com>
2022-02-16 14:48:21 -0800

    Checkpoint with brother sorting

--- src/Pages/Brothers/Brothers-Dev.tsx
+++ src/Pages/Brothers/Brothers-Dev.tsx
@@ -1,34 +1,67 @@
-import React, {Fragment} from "react";
-import "./Brothers.css"
-import Filter from "./Filter"
-import {brotherInfo} from "./brother_info.js"
-import Footer from "../../SharedComponents/Footer/Footer"
+import React, { Fragment, useEffect, useState } from "react";
+import "./Brothers.css";
+import { brotherInfo } from "./brother_info";
+import Filter from "./Filter";
+import Footer from "../../SharedComponents/Footer/Footer";
 import BrotherProfileCard from "./ProfileCards/BrotherProfileCard";
 
+function BroDev() {
+  const [brothers, setBrothers] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeFilter, setActiveFilter] = useState(0);
 
-export default class Actives extends React.Component<{}, {}> {
-    constructor(props: {}) {
-        super(props);
-    }
+  useEffect(() => {
+    const bros = JSON.parse(JSON.stringify(brotherInfo));
+    setBrothers(bros);
+    setFiltered(bros);
+  }, []);
 
-    render() {
-        return (
-            <Fragment>
-                <Filter />
-                <div className="grid-container">
-                    {brotherInfo.filter(brother => (brother.active_status === "Y")).map(brother => (
-                        <div className="grid-item"><BrotherProfileCard  id={brother.id} name={brother.name}
-                                                                        class={brother.class}
-                                                                        linkedin_url={brother.linkedin_url}
-                                                                        major={brother.major}
-                                                                        profile_url={brother.profile_url}
-                                                                        blurb = {brother.blurb}
-                                                                        casual_photo = {brother.casual_photo}/>
-                        </div>
-                    ))}
-                </div>
-                <Footer/>
-            </Fragment>
-        );
-    }
+  return (
+    <Fragment>
+      <Filter
+        brothers={brothers}
+        setFiltered={setFiltered}
+        activeFilter={activeFilter}
+        setActiveFilter={setActiveFilter}
+      />
+
+      <div className="grid-container">
+        {filtered.map((brother) => {
+          return (
+            <div className="grid-item">
+              <BrotherProfileCard
+                id={brother["id"]}
+                name={brother["name"]}
+                class={brother["class"]}
+                linkedin_url={brother["linkedin_url"]}
+                major={brother["major"]}
+                profile_url={brother["profile_url"]}
+                blurb={brother["blurb"]}
+                casual_photo={brother["casual_photo"]}
+              />
+            </div>
+          );
+        })}
+        {/*{brotherInfo*/}
+        {/*  .filter((brother) => brother.active_status === "Y")*/}
+        {/*  .map((brother) => (*/}
+        {/*    <div className="grid-item">*/}
+        {/*      <BrotherProfileCard*/}
+        {/*        id={brother.id}*/}
+        {/*        name={brother.name}*/}
+        {/*        class={brother.class}*/}
+        {/*        linkedin_url={brother.linkedin_url}*/}
+        {/*        major={brother.major}*/}
+        {/*        profile_url={brother.profile_url}*/}
+        {/*        blurb={brother.blurb}*/}
+        {/*        casual_photo={brother.casual_photo}*/}
+        {/*      />*/}
+        {/*    </div>*/}
+        {/*  ))}*/}
+      </div>
+      <Footer />
+    </Fragment>
+  );
 }
+
+export default BroDev;
--- src/Pages/Brothers/Filter.tsx
+++ src/Pages/Brothers/Filter.tsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function Filter() {
-    return(
-        <div className="filter-container">
-            <button>All</button>
-            <button>Actives</button>
-            <button>Officers</button>
-            <button>Classes</button>
-        </div>
-    )
+// @ts-ignore
+function Filter({ brothers, setFiltered, activeFilter, setActiveFilter }) {
+  useEffect(() => {
+    if (activeFilter === 0) {
+      setActiveFilter(brothers);
+      return;
+    }
+    if (activeFilter === 1) {
+      // @ts-ignore
+        const filtered = brothers.filter((brother) =>
+        brother.active_status.includes("Y")
+      );
+      setFiltered(filtered);
+    }
+    if (activeFilter === 2) {
+      setActiveFilter(brothers);
+      return;
+    }
+  }, [activeFilter]);
+
+  return (
+    <div className="filter-container">
+      <button
+        className={activeFilter === 0 ? "active" : ""}
+        onClick={() => setActiveFilter(0)}
+      >
+        Actives
+      </button>
+
+      <button
+        className={activeFilter === 1 ? "active" : ""}
+        onClick={() => setActiveFilter(1)}
+      >
+        Officers
+      </button>
+
+      <button
+        className={activeFilter === 2 ? "active" : ""}
+        onClick={() => setActiveFilter(2)}
+      >
+        Classes
+      </button>
+    </div>
+  );
 }
 
-export default Filter
+export default Filter;
