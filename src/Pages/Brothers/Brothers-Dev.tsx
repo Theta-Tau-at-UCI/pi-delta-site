d45d40c38c320d8535bcb681004bf712e433e58b
Radicitus <novivolatus@gmail.com>
2022-02-16 15:11:43 -0800

    Added basic framer-motion

--- src/Pages/Brothers/Brothers-Dev.tsx
+++ src/Pages/Brothers/Brothers-Dev.tsx
@@ -4,6 +4,7 @@ import { brotherInfo } from "./brother_info";
 import Filter from "./Filter";
 import Footer from "../../SharedComponents/Footer/Footer";
 import BrotherProfileCard from "./ProfileCards/BrotherProfileCard";
+import { AnimatePresence, motion } from "framer-motion";
 
 function BroDev() {
   const [brothers, setBrothers] = useState([]);
@@ -12,8 +13,12 @@ function BroDev() {
 
   useEffect(() => {
     const bros = JSON.parse(JSON.stringify(brotherInfo));
-    setBrothers(bros);
-    setFiltered(bros);
+    const active_bros = bros.filter(
+      (brother: { active_status: string | string[] }) =>
+        brother.active_status.includes("Y")
+    );
+    setBrothers(active_bros);
+    setFiltered(active_bros);
   }, []);
 
   return (
@@ -25,40 +30,26 @@ function BroDev() {
         setActiveFilter={setActiveFilter}
       />
 
-      <div className="grid-container">
-        {filtered.map((brother) => {
-          return (
-            <div className="grid-item">
-              <BrotherProfileCard
-                id={brother["id"]}
-                name={brother["name"]}
-                class={brother["class"]}
-                linkedin_url={brother["linkedin_url"]}
-                major={brother["major"]}
-                profile_url={brother["profile_url"]}
-                blurb={brother["blurb"]}
-                casual_photo={brother["casual_photo"]}
-              />
-            </div>
-          );
-        })}
-        {/*{brotherInfo*/}
-        {/*  .filter((brother) => brother.active_status === "Y")*/}
-        {/*  .map((brother) => (*/}
-        {/*    <div className="grid-item">*/}
-        {/*      <BrotherProfileCard*/}
-        {/*        id={brother.id}*/}
-        {/*        name={brother.name}*/}
-        {/*        class={brother.class}*/}
-        {/*        linkedin_url={brother.linkedin_url}*/}
-        {/*        major={brother.major}*/}
-        {/*        profile_url={brother.profile_url}*/}
-        {/*        blurb={brother.blurb}*/}
-        {/*        casual_photo={brother.casual_photo}*/}
-        {/*      />*/}
-        {/*    </div>*/}
-        {/*  ))}*/}
-      </div>
+      <motion.div layout className="grid-container">
+        <AnimatePresence>
+          {filtered.map((brother) => {
+            return (
+              <div className="grid-item">
+                <BrotherProfileCard
+                  id={brother["id"]}
+                  name={brother["name"]}
+                  class={brother["class"]}
+                  linkedin_url={brother["linkedin_url"]}
+                  major={brother["major"]}
+                  profile_url={brother["profile_url"]}
+                  blurb={brother["blurb"]}
+                  casual_photo={brother["casual_photo"]}
+                />
+              </div>
+            );
+          })}
+        </AnimatePresence>
+      </motion.div>
       <Footer />
     </Fragment>
   );
--- src/Pages/Brothers/ProfileCards/BrotherProfileCard.tsx
+++ src/Pages/Brothers/ProfileCards/BrotherProfileCard.tsx
@@ -1,72 +1,127 @@
 // @ts-ignore
 import React from "react";
 // @ts-ignore
-import {Button, Modal} from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
+import { motion } from "framer-motion";
 
-export default class BrotherProfileCard extends React.Component<{id: number, name: string, class: string, linkedin_url: string, major: string, profile_url:string, blurb: string, casual_photo: string}, {isOpen: boolean}> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            isOpen: false
-        };
+export default class BrotherProfileCard extends React.Component<
+  {
+    id: number;
+    name: string;
+    class: string;
+    linkedin_url: string;
+    major: string;
+    profile_url: string;
+    blurb: string;
+    casual_photo: string;
+  },
+  { isOpen: boolean }
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  openModal = () => this.setState({ isOpen: true });
+  closeModal = () => this.setState({ isOpen: false });
+
+  render() {
+    if (this.props.casual_photo === "NULL") {
+      return (
+        <motion.div
+          layout
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+        >
+          <img
+            className="headshot"
+            alt="profile-pic"
+            src={this.props.profile_url}
+            onClick={this.openModal}
+          />
+          <p className="names" onClick={this.openModal}>
+            {" "}
+            {this.props.name}{" "}
+          </p>
+          <p className="descriptor" onClick={this.openModal}>
+            {" "}
+            {this.props.major}{" "}
+          </p>
+
+          <Modal show={this.state.isOpen} onHide={this.closeModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>{this.props.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="headshot-div">
+                <img
+                  id="modal-pic"
+                  alt="casual-pic"
+                  src={this.props.profile_url}
+                />
+              </div>
+              <div>
+                <p className="blurb"> {this.props.blurb}</p>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="close-button" onClick={this.closeModal}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </motion.div>
+      );
+    } else {
+      return (
+        <motion.div
+          layout
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+        >
+          <img
+            className="headshot"
+            alt="profile-pic"
+            src={this.props.profile_url}
+            onClick={this.openModal}
+          />
+          <p className="names" onClick={this.openModal}>
+            {" "}
+            {this.props.name}{" "}
+          </p>
+          <p className="descriptor" onClick={this.openModal}>
+            {" "}
+            {this.props.major}{" "}
+          </p>
+
+          <Modal show={this.state.isOpen} onHide={this.closeModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>{this.props.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="headshot-div">
+                <img
+                  className="headshot"
+                  alt="casual-pic"
+                  src={this.props.casual_photo}
+                />
+              </div>
+              <div>
+                <p className="blurb"> {this.props.blurb}</p>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="close-button" onClick={this.closeModal}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </motion.div>
+      );
     }
-
-    openModal = () => this.setState({ isOpen: true });
-    closeModal = () => this.setState({ isOpen: false });
-
-    render() {
-        if (this.props.casual_photo === "NULL") {
-            return (
-                <div>
-                    <img className="headshot" alt = "profile-pic" src={this.props.profile_url} onClick={this.openModal}/>
-                    <p className="names" onClick={this.openModal}> {this.props.name} </p>
-                    <p className="descriptor" onClick={this.openModal}> {this.props.major} </p>
-
-                    <Modal show={this.state.isOpen} onHide={this.closeModal}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>{this.props.name}</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <div className="headshot-div">
-                                <img id = "modal-pic" alt = "casual-pic" src={this.props.profile_url}/>
-                            </div>
-                            <div>
-                                <p className="blurb"> {this.props.blurb}</p>
-                            </div>
-                        </Modal.Body>
-                        <Modal.Footer>
-
-                            <Button className="close-button" onClick={this.closeModal}>Close</Button>
-                        </Modal.Footer>
-                    </Modal>
-                </div>
-            );
-        } else{
-            return (
-                <div>
-                    <img className="headshot" alt = "profile-pic" src={this.props.profile_url} onClick={this.openModal}/>
-                    <p className="names" onClick={this.openModal}> {this.props.name} </p>
-                    <p className="descriptor" onClick={this.openModal}> {this.props.major} </p>
-
-                    <Modal show={this.state.isOpen} onHide={this.closeModal}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>{this.props.name}</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <div className="headshot-div">
-                                <img className="headshot" alt = "casual-pic" src={this.props.casual_photo}/>
-                            </div>
-                            <div>
-                                <p className="blurb"> {this.props.blurb}</p>
-                            </div>
-                        </Modal.Body>
-                        <Modal.Footer>
-
-                            <Button className="close-button" onClick={this.closeModal}>Close</Button>
-                        </Modal.Footer>
-                    </Modal>
-                </div>
-            );
-        }
-    }
+  }
 }
