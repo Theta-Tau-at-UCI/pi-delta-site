5d55f4658777c7e6b5169ecf13464ae6c3937409
Radicitus <novivolatus@gmail.com>
2022-03-07 22:11:36 -0800

    Added Data Science major

--- src/Pages/Recruitment/faq.tsx
+++ src/Pages/Recruitment/faq.tsx
@@ -7,7 +7,7 @@ class faq extends Component {
             <div id = "faq">
                 <h1> Frequently Asked Questions </h1>
                 <Question question = "What is Rush?" answer = "Rush is a week long event where Theta Tau showcases what we have to offer as an organization and what we could offer you! It’s a chance to get to know Theta Tau and its members better and see if it is the right fit for you!"/>
-                <Question question = "What Majors do Theta Tau accept?" answer = "We accept ALL engineering majors as well as Computer Science. Unfortunately, if you are not an engineering major or a Computer Science major, you will not be eligible to receive a bid."/>
+                <Question question = "What Majors do Theta Tau accept?" answer = "We accept ALL engineering majors as well as Computer Science and Data Science. Unfortunately, if you are not an engineering major, Computer Science major, or Data Science major, you will not be eligible to receive a bid."/>
                 <Question question = "What if I have more questions?" answer = "That is the perfect reason to come out to our Rush events! Ask the brothers of Theta Tau any and all of your questions or contact one of our Rush Directors!"/>
             </div>
         );
