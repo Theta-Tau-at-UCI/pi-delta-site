bdfaf2f6519a9d083080784d831bd4554d66776d
Natalie Perrochon <nperroch@uci.edu>
2024-09-25 23:52:10 +0200

    Fixed FAQ and added newest rush flyer

--- src/Pages/Recruitment/current_rush.tsx
+++ src/Pages/Recruitment/current_rush.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./current_rush.css";
+import rushflyer from "../../Media/rush/rushflyerfall2024.png";
 
 class CurrentRush extends Component {
   render() {
@@ -8,18 +9,21 @@ class CurrentRush extends Component {
         {/* <img id = "rush-header" src = {rush_header} alt = "rush_header"/> */}
         <div id="rush-blurb">
           <h1> Fall '24 Rush </h1>
-          <br />
+          <br/>
           <p className="rush-desc">
             Pi Delta will be holding our next recruitment event from September 30th to October 4th, 2024!
             We would love to meet you!
-            Details are in the graphic below and on our Instagram <a href={"https://www.instagram.com/thetatauuci"} rel={"noreferrer"} target={"_blank"}>@thetatauuci</a>.
+            Details are in the graphic below and on our Instagram <a href={"https://www.instagram.com/thetatauuci"}
+                                                                     rel={"noreferrer"}
+                                                                     target={"_blank"}>@thetatauuci</a>.
             <br></br><br></br>If you have any questions in the meantime, feel free to contact our
             recruitment directors.
-            <br />
+            <br/>
           </p>
+          <img className="media" src={rushflyer} alt="rushflyer"/>
           <p className="rush-desc">
             Rush Chair: Ellen So
-            <br />
+            <br/>
             Rush Chair: Miguel Murillo | (916) 895-0339
           </p>
         </div>
--- src/Pages/Recruitment/faq.tsx
+++ src/Pages/Recruitment/faq.tsx
@@ -8,7 +8,7 @@ class faq extends Component {
                 <h1> Frequently Asked Questions </h1>
                 <Question question = "What is Rush?" answer = "Rush is a week long event where Theta Tau showcases what we have to offer as an organization and what we could offer you! It’s a chance to get to know Theta Tau and its members better and see if it is the right fit for you!"/>
                 <Question question = "What Majors do Theta Tau accept?" answer = "We accept ALL engineering majors as well as Computer Science and Data Science. Unfortunately, if you are not an engineering major, Computer Science major, or Data Science major, you will not be eligible to receive a bid."/>
-                <Question question = "How can I prepare for rush?" answer = ""/>
+                <Question question = "How can I prepare for rush?" answer = "Come with an open mind and be ready to meet lots of people– not just actives, but fellow rushees too! We recommend preparing your resume and updating your LinkedIn before ProDev night, so you can get the best experience possible. Also, it might get cold, since rush can last a while- be sure to bring a jacket! "/>
                 <Question question = "What if I have more questions?" answer = "That is the perfect reason to come out to our Rush events! Ask the brothers of Theta Tau any and all of your questions or contact one of our Rush Directors!"/>
             </div>
         );
